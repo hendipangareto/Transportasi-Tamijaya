b00bc6f94e93b42655e916f9cfc80c757b1286b3
hendipangareto <hendipangareto01@gmail.com>
2023-09-12 11:46:51 +0700

    master logistik toko

--- app/Http/Controllers/Admin/MasterLogistik/TokoController.php
+++ app/Http/Controllers/Admin/MasterLogistik/TokoController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Admin\MasterLogistik;
 
-class TokoController
-{
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
+class TokoController extends Controller
+{
+    public function getToko (){
+        return view('admin.master-logistik.toko.list-toko');
+    }
 }
