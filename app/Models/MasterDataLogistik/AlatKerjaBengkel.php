a9edab5b96b310ab8e0e4120d31df502379bf095
hendipangareto <hendipangareto01@gmail.com>
2023-09-19 10:12:28 +0700

    MasterLogistik/AlatKerjaBengkel/model

---
+++ app/Models/MasterDataLogistik/AlatKerjaBengkel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\MasterDataLogistik;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AlatKerjaBengkel extends Model
+{
+    protected $table = 'alat_kerja_bengkels';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'kode_alat_kerja_bengkel',
+        'nama_alat_kerja_bengkel',
+        'satuan_id',
+
+
+    ];
+
+}
+
