e14a012889bd1391d1a9c55d5df089b24b25a8b2
hendipangareto <hendipangareto01@gmail.com>
2023-09-19 17:10:03 +0700

    MasterKeuangan/TipeAset/Model

--- app/Http/Controllers/Admin/MasterKeuangan/Aset/TipeAsetController.php
+++ app/Http/Controllers/Admin/MasterKeuangan/Aset/TipeAsetController.php
@@ -4,16 +4,33 @@ namespace App\Http\Controllers\Admin\MasterKeuangan\Aset;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\MasterKeuangan\TipeAset;
 class TipeAsetController extends Controller
 {
     public function getTipeAset()
     {
-        return view('admin.master-keuangan.aset.tipe-aset.list-tipe-aset');
+        $TipeAset = TipeAset::all();
+        return view('admin.master-keuangan.aset.tipe-aset.list-tipe-aset', compact('TipeAset'));
     }
 
     public function TambahTipeAset(Request $request)
     {
+        $TipeAset = new TipeAset();
+        $lastNomor = TipeAset::orderBy('id', 'desc')->first();
+        $lastNumber = $lastNomor ? intval(substr($lastNomor->kode_tipe_aset, -2)) : 0;
+        $newNumber = $lastNumber + 1;
+        $noTipeAset = 'TA-01' . str_pad($newNumber, 2, '0', STR_PAD_LEFT);
 
+        $TipeAset->kode_tipe_aset = $TipeAset;
+        $TipeAset->nama_tipe_aset = $request->nama_tipe_aset;
+        $TipeAset->deskripsi_tipe_aset = $request->deskripsi_tipe_aset;
+
+        dd($TipeAset);
+        try {
+            $TipeAset->save();
+            return redirect(route('master-keuangan.aset.tipe-aset'))->with('pesan-berhasil', 'Anda berhasil menambah data tipe aset');
+        } catch (\Exception $e) {
+            return redirect(route('master-keuangan.aset.tipe-aset'))->with('pesan-gagal', 'Anda gagal menambah data tipe aset');
+        }
     }
 }
---
+++ app/Models/MasterKeuangan/TipeAset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\MasterKeuangan;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipeAset extends Model
+{
+    protected $table = 'tipe_asets';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'kode_tipe_aset',
+        'nama_tipe_aset',
+        'deskripsi_tipe_aset',
+
+    ];
+}
+
