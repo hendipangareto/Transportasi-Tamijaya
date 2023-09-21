6a1c8c5f7ad2b08afd35ab01c673eb8b6265fc2b
hendipangareto <hendipangareto01@gmail.com>
2023-09-21 09:20:27 +0700

    migration/metodepenyusutan

--- app/Http/Controllers/Admin/MasterKeuangan/MetodePenyusutanController.php
+++ app/Http/Controllers/Admin/MasterKeuangan/MetodePenyusutanController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin\MasterKeuangan;
 
 use App\Http\Controllers\Controller;
+use App\Models\MasterKeuangan\MetodePenyusutan;
 
 class MetodePenyusutanController extends Controller
 {
     public function getMetodePenyusutan ()
     {
+        $MetodePenyusutan = MetodePenyusutan::all();
         return view('admin.master-keuangan.metode-penyusutan.list-metode-penyusutan');
     }
 }
